2e19d980cdc055897db346dbc95cd0346549263e
Andrew Lapteff <this.is.amigogo@gmail.com>
2023-07-03 13:38:13 +0300

    Added categories

---
+++ src/components/Categories.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import s from '../assets/stylesheets/categiries.module.scss'
+import { categories } from '../data'
+import CategoryItem from './CategoryItem'
+
+const Categories = () => {
+  return (
+    <div className={s['categiries-container']}>
+      {categories.map((category) => {
+        return <CategoryItem key={category.id} item={category} />
+      })}
+    </div>
+  )
+}
+
+export default Categories
---
+++ src/components/CategoryItem.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import s from '../assets/stylesheets/categoryItem.module.scss'
+import Category from '../types/category'
+
+const CategoryItem = ({ item }: { item: Category }) => {
+  return (
+    <div className={s['category-item']}>
+      <img src={item.img} alt="category-image" />
+      <div className={s.info}>
+        <h3>{item.title}</h3>
+        <button>SHOP NOW</button>
+      </div>
+    </div>
+  )
+}
+
+export default CategoryItem
