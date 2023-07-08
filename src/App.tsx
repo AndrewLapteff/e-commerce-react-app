0678b197cf774b32a7bad8b8b91a646a89dc5fd2
Andrew Lapteff <this.is.amigogo@gmail.com>
2023-07-08 19:29:49 +0300

    Added product page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Home from './pages/Home'
 import ProductsList from './pages/ProductsList'
+import Product from './pages/Product'
 
 const App = () => {
-  return <ProductsList />
+  return <Product />
 }
 
 export default App
---
+++ src/pages/Product.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../components/Layout/Navbar'
+import Footer from '../components/Layout/Footer'
+import ProductInfo from '../components/ProductPage/ProductInfo'
+
+const Product = () => {
+  return (
+    <>
+      <Navbar />
+      <ProductInfo />
+      <Footer />
+    </>
+  )
+}
+
+export default Product
